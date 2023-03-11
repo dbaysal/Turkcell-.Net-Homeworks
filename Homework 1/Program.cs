c0fceec4a18e5e4ede9ebb26f0962b1036d5d5b0
Doğukan Baysal <dbaysal2001@gmail.com>
2023-03-11 16:07:52 +0200

    Proje dosyası ekle.

---
+++ Homework 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+int[] array = { 13, 14, 34, 1, 10, 5, 4, 20 };
+
+int max = Int32.MinValue;
+int min = Int32.MaxValue;
+int i;
+
+//Finding min and max with for loop
+for (i = 0; i < array.Length; i++)
+{
+    max = max < array[i] ? array[i] : max;
+    min = min > array[i] ? array[i] : min;
+}
+
+
+//Finding min and max with while loop
+max = Int32.MinValue;
+min = Int32.MaxValue;
+i = 0;
+while(i < array.Length)
+{
+    max = max < array[i] ? array[i] : max;
+    min = min > array[i] ? array[i] : min;
+    i++;
+}
+
+
+//Finding min and max with foreach loop
+max = Int32.MinValue;
+min = Int32.MaxValue;
+foreach (int item in array)
+{
+    max = max < item ? item : max;
+    min = min > item ? item : min;
+}
+
+
+//Average operation will only be done to last min and max value as all of them are same
+double average = (double)(max + min) / 2;
+
+Console.WriteLine($"Minimum value is: {min}");
+Console.WriteLine($"Maximum value is: {max}");
+Console.WriteLine($"Average of min and max value is: {average}");
+
+
