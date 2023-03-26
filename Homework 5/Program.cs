5cf4b39619f5c769d52089d9a73b09771287d3aa
Doğukan Baysal <dbaysal2001@gmail.com>
2023-03-26 20:19:42 +0300

    Homework 5: SRP & OCP Principles with an example.

--- Homework 5/Program.cs
+++ Homework 5/Program.cs
@@ -11,38 +11,12 @@ namespace Homework_5
     {
         static void Main(string[] args)
         {
+            string instruction = "00100100001";
+            int result = Decoder.Decode(instruction);
+            Console.WriteLine($"Intruction : {instruction}");
+            Console.WriteLine($"Result : {result}");
         }
 
-        public static IOperate Decoder(string instruction)
-        {
-            string OpCode = GetOpCode(instruction);
-            IOperate? operationClass = GetOperationClass(OpCode);
-
-            
-
-        }
-
-        private static IOperate GetOperationClass(string opCode)
-        {
-            //Get all classes that are implementing IOperate interface to enable adding new instructions without changing the code
-            foreach (Type mytype in System.Reflection.Assembly.GetExecutingAssembly().GetTypes()
-                 .Where(mytype => mytype.GetInterfaces().Contains(typeof(IOperate))))
-            {
-                IOperate op = (IOperate)mytype;
-                string TypeOpcode = op.GetOpCode();
-
-                if (opCode.Equals(TypeOpcode))
-                {
-                    return (IOperate)Activator.CreateInstance(mytype);
-                }
-
-            }
-            return null;
-        }
-
-        public static string GetOpCode(string instruction)
-        {
-            return instruction.Substring(0, 3);
-        }
+        
     }
 }
